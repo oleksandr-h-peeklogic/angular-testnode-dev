fcb4223e9d8d65d319f2d79c673af36ffd56c3c4
ibegei0208 <ibegei0208>
2019-10-18 14:43:35 +0300

    make it better

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.css']
 })
-export class TestComponent implements OnInit {
 
-  constructor() { }
+export class TestComponent implements OnInit {
+  id = '';
+  
+  constructor( private route: ActivatedRoute,
+    private location: Location ) {
+    this.id = this.route.snapshot.paramMap.get('id');
+   }
 
   ngOnInit() {
   }
