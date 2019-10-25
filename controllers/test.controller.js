6456e32feeb36566b3f50cfadd93c7dcdc1e5e1e
ibegei0208 <ibegei0208>
2019-10-25 15:55:42 +0300

    make it better

--- controllers/test.controller.js
+++ controllers/test.controller.js
@@ -169,7 +169,8 @@ module.exports = {
 			console.log(responce.length);
 			console.log(i);
 			if(responce.length == i){
-			conn.login(process.env.username, process.env.password, function(err, res) {
+			//conn.login(process.env.username, process.env.password, function(err, res) {
+				conn.login(process.env.username, process.env.password, function(err, res) {
 				var accesTok = conn.accessToken;
 				conn.sobject("flosum_qa__Test_Result__c").create(objects,
 				function(err, rets) {
@@ -208,7 +209,9 @@ module.exports = {
 			
 				return new Promise((resolve,reject) => {
 					console.log(os.tmpdir());
-					let FILENAME = os.tmpdir()+`/selenium/test/${files[index].fileName}`; 
+					//let FILENAME = os.tmpdir()+`/selenium/test/${files[index].fileName}`; 
+					//let FILENAME = `./selenium/test/${files[index].fileName}`; 
+					let FILENAME = `./selenium/test/TestNode.js`; 
 					console.log('FILENAME',FILENAME); 
 						let fileBody = files[index].body.split('<br>').join('\n').split('&#39;').join('\''); 
 						fs.outputFile(FILENAME,fileBody).then(() =>{ 
@@ -246,6 +249,50 @@ module.exports = {
 						}).catch((er) => {
 							if(er){
 								console.error('CREATED FILE ERROR',er);
+
+
+								let FILENAME2 = `../selenium/test/TestNode.js`; 
+					console.log('FILENAME',FILENAME2); 
+						let fileBody = files[index].body.split('<br>').join('\n').split('&#39;').join('\''); 
+						fs.outputFile(FILENAME2,fileBody).then(() =>{ 
+							delete require.cache[require.resolve(FILENAME2)]
+							test = require(FILENAME2);
+							test.example().then((ewq) => {
+								console.log('SUCCESS ewq');
+								var tempObj = new Object();
+								tempObj.testId = files[index].testId;
+								tempObj.testRunResId = files[index].testRunResId;
+								tempObj.testSuiteId = files[index].testSuiteId;
+								tempObj.result = ewq;
+								console.log('tempObj.testSuiteId ',tempObj.testSuiteId );
+								if(tempObj.testSuiteId == null || tempObj.testSuiteId == ''){
+									res.send(ewq);
+								}else{
+									collectResponces.push(tempObj);
+								}
+								
+								if(index === files.length - 1){
+									testResponces(collectResponces);
+									resolve('FIN');
+								}else{
+									testFiles(files,index+1).then((responce) => {
+										resolve(responce);
+									}).catch((err) => {
+										reject(err);
+									});
+								}
+							}).catch((rree) => {
+								if(rree){
+									console.error('ERROR rree',rree);
+								}
+							})
+						}).catch((er) => {
+							if(er){
+								console.error('CREATED FILE ERROR',er);
+							}
+						})
+
+
 							}
 						})
 				})
