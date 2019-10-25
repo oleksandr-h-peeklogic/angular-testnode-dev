7093de39b5aa5851cb72aa96850a36a58dfe7f15
dosiakbob <dosiakbohdan@gmail.com>
2019-10-25 14:52:43 +0300

    make it better

--- controllers/test.controller.js
+++ controllers/test.controller.js
@@ -207,7 +207,7 @@ module.exports = {
 			async function testFiles(files,index){
 			
 				return new Promise((resolve,reject) => {
-					let FILENAME = `selenium/test/${files[index].fileName}`; 
+					let FILENAME = `/selenium/test/${files[index].fileName}`; 
 					console.log('FILENAME',FILENAME); 
 						let fileBody = files[index].body.split('<br>').join('\n').split('&#39;').join('\''); 
 						fs.outputFile(FILENAME,fileBody).then(() =>{ 
@@ -254,4 +254,3 @@ module.exports = {
 
 	}
 };
-module.exports.runtests = this.runtests;
